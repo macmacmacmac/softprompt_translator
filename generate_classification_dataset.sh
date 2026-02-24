41dca0de7fd286f339e39652dd0b6953be850d5a
Suryansh Goyal <suryanshg47@gmail.com>
2026-02-24 14:32:00 -0500

    Added a slurm script for running generation code for DoD

---
+++ generate_classification_dataset.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --mem=16g
+#SBATCH -J "Gen DoD"
+#SBATCH -p short
+#SBATCH -t 24:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -C A100
+#SBATCH -o logs.out
+#SBATCH -e logs.out
+
+# -----------------------------
+# Load Required Modules
+# -----------------------------
+module load python/3.12.3
+module load cuda/12.9.0
+
+# -----------------------------
+# Create / Activate venv
+# -----------------------------
+VENV_DIR=".venv"
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    python -m venv "$VENV_DIR"
+    source "$VENV_DIR/bin/activate"
+    pip install --upgrade pip
+    pip install -r requirements.txt
+else
+    echo "Activating existing virtual environment..."
+    source "$VENV_DIR/bin/activate"
+fi
+
+# -----------------------------
+# Generate Classification Datasets
+# -----------------------------
+python -m run_experiment --scripts classification_dataset_generator
